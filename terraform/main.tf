c2c43f717f2c3b0e21f7a1edc2db96e4d21e58c6
John Driggers-McDowall <j.driggers-mcdowall@accenture.com>
2025-05-14 14:05:29 +0100

    add Gitlab CI/CD pipeline file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "aws_static_website" {
   source = "modules/aws-static-website"
-  ami = var.ami
-  instance_type = var.instance_type
+  ami = "ami-e7768380"
+  instance_type = "t2.small"
   user_data_file = file("${path.module}/scripts/user_data.sh")
 }
